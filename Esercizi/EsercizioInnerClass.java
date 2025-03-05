168c2ea8453ad1c6b7158999a4b00ed45d85e8b8
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-05 17:43:23 +0100

    Update EsercizioInnerClass.java

--- Esercizi/EsercizioInnerClass.java
+++ Esercizi/EsercizioInnerClass.java
@@ -6,11 +6,19 @@ class Adulto {
         this.nome = nome;
     }
 
+    public void stampaAdulto(){
+        System.out.println("adulto: " + this.nome)
+    }
+
     public class Figlio {
         String nomeFiglio;
         
         public Figlio(String nomeFiglio) {
             this.nomeFiglio = nomeFiglio;
+        }
+
+        public void stampaFiglio(){
+            System.out.println("figlio: " + this.nomeFiglio)
         }
     }
 
@@ -18,10 +26,12 @@ class Adulto {
 
 public class EsercizioInnerClass {
     public static void main(String[] args) {
-        // Creazione degli oggetti Adulto
-        Adulto genitore1 = new Adulto("Topolino");
-        Adulto.Figlio figlio1 = genitore1.new Figlio("Pippo");
-        // Creazione dei figli per ogni genitore
-        System.out.println("Famiglia di " + genitore1.nome + ", Figlio:" + figlio1.nomeFiglio);
+        Adulto adulto1 = new Adulto("Topolino");
+        Adulto.Figlio figlio1 = adulto1.new Figlio("Pippo");
+        
+        System.out.println("Famiglia di " + adulto1.nome + ", Figlio:" + figlio1.nomeFiglio);
+
+        adulto1.stampaAdulto();
+        figlio1.stampaFiglio();
     }
 }
